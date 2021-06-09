fa8a2947071559343008047877443397405e39f4
vanessabonis <vanessa1302@gmail.com>
2021-06-09 16:58:03 -0300

    apaguei o delta

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double a, b, c, delta, x1, x2;
+		double a, b, c, x1, x2;
 		
 		System.out.print("Valor de a: ");
 		a = sc.nextDouble();
@@ -18,9 +18,9 @@ public class Program {
 		System.out.print("Valor de c: ");
 		c = sc.nextDouble();
 		
-		delta = Math.pow(b, 2) - 4*a*c;
-		x1 = (- b + Math.sqrt(delta))/2*a;
-		x2 = (- b - Math.sqrt(delta))/2*a;
+		
+		x1 = (- b + Math.sqrt((b*b) - 4*a*c))/2*a;
+		x2 = (- b - Math.sqrt((b*b) - 4*a*c))/2*a;
 
 		System.out.printf("X1 = %.2f\nX2 = %.2f\n", x1,x2);
 		
